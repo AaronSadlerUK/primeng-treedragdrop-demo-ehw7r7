bf3bf5b1be2d430ffa3c0b11ce06a7520f256bf7
AaronSadlerUK <aaron@aaronsadler.uk>
2022-12-07 16:09:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeng-treedragdrop-demo-ehw7r7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-treedragdrop-demo-hqv6zp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import {Component,OnInit} from '@angular/core';
+import {NodeService} from './nodeservice';
+import {TreeNode} from 'primeng/api';
+import {TreeDragDropService} from 'primeng/api';
+import {MessageService} from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+    providers: [TreeDragDropService,MessageService],
+    styles:[`
+        h4 {
+            text-align: center;
+            margin: 0 0 8px 0;
+        }
+    `]
+})
+export class AppComponent { 
+    files1: TreeNode[];
+
+    files2: TreeNode[];
+
+    files3: TreeNode[];
+
+    files4: TreeNode[];
+    
+    constructor(private nodeService: NodeService) { }
+
+    ngOnInit() {
+        this.nodeService.getFiles().then(files => this.files1 = files);
+        this.nodeService.getFiles().then(files => this.files2 = files);
+        this.files3 = [{
+                label: "Backup",
+                data: "Backup Folder",
+                expandedIcon: "pi pi-folder-open",
+                collapsedIcon: "pi pi-folder"
+            }
+        ];
+
+        this.files4 = [{
+                label: "Storage",
+                data: "Storage Folder",
+                expandedIcon: "pi pi-folder-open",
+                collapsedIcon: "pi pi-folder"
+            }
+        ];
+    }
+}
